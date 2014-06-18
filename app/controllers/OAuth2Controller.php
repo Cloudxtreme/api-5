b557d2f247957b6e17b2a532399c99c88b2fe904
Roberto Silva <robertos@agap2.pt>
2014-06-18 14:53:00 +0100

    postAccessToken()

--- app/controllers/OAuth2Controller.php
+++ app/controllers/OAuth2Controller.php
@@ -57,7 +57,7 @@ class OAuth2Controller extends \BaseController {
 		// Initiate the request handler which deals with $_GET, $_POST, etc
 		$request = new \League\OAuth2\Server\Util\Request();
 		
-		/*
+		
 		// Create the auth server, the three parameters passed are references
 		// to the storage models
 		$this->authserver = new \League\OAuth2\Server\Authorization(
@@ -70,16 +70,14 @@ class OAuth2Controller extends \BaseController {
 		// Enable the authorization code grant type
 		//$this->authserver->addGrantType(new \League\OAuth2\Server\Grant\AuthCode($this->authserver));
 		//$this->authserver->addGrantType();
-		 */
-		
-		
+				
 		// Create the auth server, the three parameters passed are references
 		//  to the storage models
-		$this->authserver = new League\OAuth2\Server\Authorization(
+		/*$this->authserver = new League\OAuth2\Server\Authorization(
 				new ClientModel,
 				new SessionModel,
 				new ScopeModel
-		);
+		);*/
 		
 		// Enable the authorization code grant type
 		$this->authserver->addGrantType(new League\OAuth2\Server\Grant\AuthCode());
@@ -103,17 +101,62 @@ class OAuth2Controller extends \BaseController {
 		// get the data from the check-authorization-params filter
 		$params = Session::get('authorize-params');
 		
+		//echo var_export(Auth::user()); die('OAuth2Controller.getAuthorize');
+		//echo var_export($params); die('OAuth2Controller.getAuthorize');
+		
 		// get the user id
-		$params['user_id'] = Auth::user()->id;
+		//$params['user_id'] = Auth::user()->id;
 		
 		// display the authorization form
-		return View::make('authorization-form', array('params' => $params));
+		//return View::make('authorization-form', array('params' => $params));
+		
+		// Generate an authorization code
+		$code = $this->authserver->getGrantType('authorization_code')->newAuthoriseRequest('user', $params['user_id'], $params);
+		
+		// Redirect the user back to the client with an authorization code
+		return Redirect::to(
+			League\OAuth2\Server\Util\RedirectUri::make($params['redirect_uri'],
+				array(
+					'code'  =>  $code,
+					'state' =>  isset($params['state']) ? $params['state'] : ''
+				)
+			));
 		
 		//return '» OAUTH2 Authorize';
 	}
 	
 	public function postAccessToken()
 	{
+		
+		try {
+		
+			// Tell the auth server to issue an access token
+			$response = $this->authserver->issueAccessToken();
+			
+			var_export($response); die('OAuth2Controller.postAccessToken');
+		
+		} catch (\League\OAuth2\Server\Exception\ClientException $e) {
+		
+			// Throw an exception because there was a problem with the client's request
+			$response = array(
+					'error' =>  $this->authserver->getExceptionType($e->getCode()),
+					'error_description' => $e->getMessage()
+			);
+		
+			// Set the correct header
+			@header($this->authserver->getExceptionHttpHeaders($this->authserver->getExceptionType($e->getCode())));
+		
+		} catch (Exception $e) {
+		
+			// Throw an error when a non-library specific exception has been thrown
+			$response = array(
+					'error' =>  'undefined_error',
+					'error_description' => $e->getMessage()
+			);
+		}
+		
+		@header('Content-type: application/json');
+		echo json_encode($response);
 		
 		
 		
@@ -122,9 +165,11 @@ class OAuth2Controller extends \BaseController {
 		//return '» POST OAUTH2 Access Token';
 	}
 
-	public function getAccesstokens()
+	public function getAccesstoken()
 	{
-		return '» OAUTH2 AccessTokens';
+		
+		
+		return '» OAUTH2 AccessToken';
 	}
 	
 	public function missingMethod($parameters = array())
