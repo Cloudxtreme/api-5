97e1b8d1864349b7a075d76316a82947123854c3
Thijs Van der Schaeghe <thijs@catlab.be>
2014-08-27 17:04:54 +0200

    Routing update.

--- app/routes.php
+++ app/routes.php
@@ -456,7 +456,7 @@ Route::any('oauth2/{path?}', function()
 })->where ('path', '.+');;
 
 // The All Catching One
-Route::match (array ('GET', 'POST', 'PATCH', 'PUT', 'DELETE'), '{path?}', array ('before' => 'oauth2'), function ($path) {
+Route::match (array ('GET', 'POST', 'PATCH', 'PUT', 'DELETE'), '{path?}', array (), function ($path) {
 
 	$verifier = \bmgroup\OAuth2\Verifier::getInstance ();
 	if (!$verifier->isValid ())
