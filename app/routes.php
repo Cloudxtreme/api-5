ef93f2711da4474a264eba8b52d2010673105aa1
Thijs Van der Schaeghe <thijs@catlab.be>
2014-09-02 12:52:44 +0200

    Storing access token in cookie to avoid "the zombie login".

--- app/routes.php
+++ app/routes.php
@@ -249,6 +249,8 @@ Route::any('login/{path?}', 'LoginController@login')->where ('path', '.+');
 Route::any('logout/{path?}', 'LoginController@logout')->where ('path', '.+');
 Route::get ('1/version', 'ProxyController@guest');
 
+Route::get ('authenticate/{path?}', 'ProxyController@guest')->where ('path', '.+');
+
 Route::any('oauth2/{path?}', 'Oauth2Controller@dispatch')->where ('path', '.+');;
 
 // Reseller endpoints
