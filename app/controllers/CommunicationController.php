7582759d8e341d1cd517f4df1af0a5f272e8ca4e
Pedro Silva <pedrodee@gmail.com>
2014-09-05 10:25:27 +0100

    template system

--- app/controllers/CommunicationController.php
+++ app/controllers/CommunicationController.php
@@ -16,7 +16,8 @@ class CommunicationController extends BaseController {
             $password = $data['password'];
             // send request to engine via gearman
             $output = App::make ('cwclient')->login ($username, $password);
-            return View::make('signin.login', $output);
+            return print_r( $output );
+//            return View::make('signin.login', $output);
         } else {
             return View::make('signin.login', $data);
         }
@@ -47,8 +48,8 @@ class CommunicationController extends BaseController {
         $data = array();
         $message = new Clickatell;
 
-        $status = $message->to(351966479598)
-            ->message('Hello world!')
+        $status = $message->to(32476858168)
+            ->message('CW is watching you!')
             ->send();
 
 		return print_r($status, true);
