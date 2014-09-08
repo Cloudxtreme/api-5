19b5fed5aea624436d963b7b139cf4097d807d31
Pedro Silva <pedrodee@gmail.com>
2014-09-08 16:27:37 +0100

    comment new templates

--- app/controllers/LoginController.php
+++ app/controllers/LoginController.php
@@ -29,6 +29,23 @@ class LoginController extends BaseController {
 		}
 
 		exit;
+
+//        $data = Input::all();
+//
+//        if(isset($data['email']) && isset($data['password'])){
+//            $username = $data['email'];
+//            $password = $data['password'];
+//            // send request to engine via gearman
+//            $output = App::make ('cwclient')->login ($username, $password);
+//            // if ok redirect
+//            if(isset($output['id'])){
+//                return Redirect::to('http://devplatform.cloudwalkers.be');
+//            } else {
+//                return View::make('signin.login', $output);
+//            }
+//        } else {
+//            return View::make('signin.login', $data);
+//        }
 	}
 
     public function logout ()
@@ -44,29 +61,29 @@ class LoginController extends BaseController {
 	
 	public function register ()
 	{
-//		$response = $this->ancientFrontController->dispatch ($this->ancientPage);
-//
-//		if ($response)
-//		{
-//			$response->output ();
-//		}
-//
-//		exit;
+		$response = $this->ancientFrontController->dispatch ($this->ancientPage);
 
-        $data = Input::all();
+		if ($response)
+		{
+			$response->output ();
+		}
 
-        if( !empty($data) ){
-            // send request to engine via gearman
-            $output = App::make ('cwclient')->register ($data['email'], $data['password'], $data['firstname'], $data['name']);
-            // if ok redirect
-            if(isset($output['success'])){
-                return Redirect::to('login');
-            } else {
-                return View::make('signin.register', $output);
-            }
-        } else {
-            return View::make('signin.register', $data);
-        }
+		exit;
+
+//        $data = Input::all();
+//
+//        if( !empty($data) ){
+//            // send request to engine via gearman
+//            $output = App::make ('cwclient')->register ($data['email'], $data['password'], $data['firstname'], $data['name']);
+//            // if ok redirect
+//            if(isset($output['success'])){
+//                return Redirect::to('login');
+//            } else {
+//                return View::make('signin.register', $output);
+//            }
+//        } else {
+//            return View::make('signin.register', $data);
+//        }
 
 	}
 
