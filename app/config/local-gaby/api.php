d41f4e6fba473f7f6040be66861d4e1c662ca1b6
gabriela.raducan@arnia.ro <gabriela.raducan@arnia.ro>
2014-09-10 16:01:26 +0300

    Added local env

---
+++ app/config/local-gaby/api.php
@@ -0,0 +1,24 @@
+<?php
+
+return array(
+
+	/*
+	|--------------------------------------------------------------------------
+	| Synchronized
+	|--------------------------------------------------------------------------
+	| If set, the API will call the worker directly, instead of using a jobserver.
+	*/
+	
+	'synchronized' => true,
+	
+	/*
+	|--------------------------------------------------------------------------
+	| Engine path
+	|--------------------------------------------------------------------------
+	| This path is used by the local Jobserver to sync a queue request.
+	*/
+	'engine' => array(
+	
+		'workerpath' => '/var/www/cloudwalkers-engine.local/public_html/workers/'
+	)
+);
