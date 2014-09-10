47888c892813d182b69a8129db2579ca2fe4c65a
Pedro Silva <pedrodee@gmail.com>
2014-09-10 19:12:09 +0100

    template login and register

--- app/controllers/LoginController.php
+++ app/controllers/LoginController.php
@@ -22,30 +22,30 @@ class LoginController extends BaseController {
 	
 	public function login ()
 	{
-//		$response = $this->ancientFrontController->dispatch ($this->ancientPage);
-//		if ($response)
-//		{
-//			$response->output ();
-//		}
+		$response = $this->ancientFrontController->dispatch ($this->ancientPage);
+		if ($response)
+		{
+			$response->output ();
+		}
+
+		exit;
+
+//        $data = Input::all();
 //
-//		exit;
-
-        $data = Input::all();
-
-        if(isset($data['email']) && isset($data['password'])){
-            $username = $data['email'];
-            $password = $data['password'];
-            // send request to engine via gearman
-            $output = App::make ('cwclient')->login ($username, $password);
-            // if ok redirect
-            if(isset($output['id'])){
-                return Redirect::to('http://devplatform.cloudwalkers.be');
-            } else {
-                return View::make('signin.login', $output);
-            }
-        } else {
-            return View::make('signin.login', $data);
-        }
+//        if(isset($data['email']) && isset($data['password'])){
+//            $username = $data['email'];
+//            $password = $data['password'];
+//            // send request to engine via gearman
+//            $output = App::make ('cwclient')->login ($username, $password);
+//            // if ok redirect
+//            if(isset($output['id'])){
+//                return Redirect::to('http://devplatform.cloudwalkers.be');
+//            } else {
+//                return View::make('signin.login', $output);
+//            }
+//        } else {
+//            return View::make('signin.login', $data);
+//        }
 	}
 
     public function logout ()
@@ -61,29 +61,37 @@ class LoginController extends BaseController {
 	
 	public function register ()
 	{
-//		$response = $this->ancientFrontController->dispatch ($this->ancientPage);
-//
-//		if ($response)
-//		{
-//			$response->output ();
-//		}
-//
-//		exit;
+		$response = $this->ancientFrontController->dispatch ($this->ancientPage);
 
-        $data = Input::all();
+		if ($response)
+		{
+			$response->output ();
+		}
 
-        if( !empty($data) ){
-            // send request to engine via gearman
-            $output = App::make ('cwclient')->register ($data['email'], $data['password'], $data['firstname'], $data['name']);
-            // if ok redirect
-            if(isset($output['success'])){
-                return Redirect::to('http://devplatform.cloudwalkers.be');
-            } else {
-                return View::make('signin.register', $output);
-            }
-        } else {
-            return View::make('signin.register', $data);
-        }
+		exit;
+
+//        $data = Input::all();
+//		$param = Request::segment(2);
+//		$invitation = Request::segment(3);
+
+//		return $param . ' - ' . $invitation; exit;
+//
+//        if( !empty($data) || isset($data['invitation'])){
+//            // send request to engine via gearman
+//            $output = App::make ('cwclient')->register ($data['email'], $data['password'], $data['firstname'], $data['name']);
+//            // if ok redirect
+//            if(isset($output['success'])){
+//                return Redirect::to('http://devplatform.cloudwalkers.be/login.html');
+//            } else {
+//                return View::make('signin.register', $output);
+//            }
+//        } else {
+//	        if($data['email']){
+//                return View::make('signin.register', $data);
+//	        } else {
+//		        App::abort(403, 'Unauthorized action.');
+//	        }
+//        }
 
 	}
 
