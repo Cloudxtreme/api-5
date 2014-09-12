472949be24cd8b962185fd1509c725d91e0021e0
Koen Betsens <koen@tick.ee>
2014-09-12 09:55:53 +0200

    v1.1 API updates

--- app/controllers/ContactController.php
+++ app/controllers/ContactController.php
@@ -3,30 +3,37 @@
 class ContactController extends BaseController {
 
 	/**
-	 *	Get Contact
-	 *	Based on ID
+	 *	Get Contact by id.
+	 *	The response validation is set in schemas.
+	 *
+	 *	@return array
 	 */
 	public function get($accountId = null, $contactId = null)
 	{
 		// Add path attributes
-		Input::merge(array('accountId'=> $accountId, 'id'=> $contactId));
+		$input = self::prepInput(array('id'=> $contactId, 'accountId'=> $accountId));
 		
 		// Validation rules
 		$rules = array('id'=> 'required|integer');
 		
 		// Validate
-		$validator = Validator::make(Input::all(), $rules);
+		$validator = Validator::make($input, array_merge(self::$inputRules, $rules));
+
+		//return $validator->getValidated();
 		
 		// Check if the validator failed
 		if ($validator->fails())
 		
 			return Redirect::to(400)->withErrors($validator);
 		
+		
+		// Request jobload
+		$jobload = (object) array('controller'=> 'ContactController', 'action'=> 'get', 'payload'=> array_intersect_key(Input::all(), $rules));
+		
 		// Request Foreground Job
-		$payload = (object) array('controller'=> 'ContactController', 'action'=> 'get', 'payload'=> array_intersect_key(Input::all(), $rules));
+		$response = self::jobdispatch ('controllerDispatch', $jobload);
 		
-		$response = self::jobdispatch ('controllerDispatch', $payload);
-		
+		// Return schema based response
 		return SchemaValidator::validate (json_decode($response, true), 'contact')->intersect;
 	}
 
--- app/lib/SchemaValidator.php
+++ app/lib/SchemaValidator.php
@@ -1,22 +1,46 @@
 <?php
 
-class SchemaValidator
+class SchemaValidator extends Validator
 {
-
+	
+	/**
+	 *	$intersect holds the actually validated input.
+	 */
+	public $intersect;
+	
+	/**
+	 *	Validate schema
+	 *
+	 *	extracts rules from model schema json file.
+	 *	if no json is found, 0 rules will be validated.
+	 *
+	 *	@return Validator
+	 */
 	public static function validate($input, $model)
 	{
 		$schema = Config::get('api.schemapath') . $model . '.json';
 		
 		$rules = file_exists ($schema)?
 		
-			json_decode( file_get_contents ($schema), true):
+			json_decode (file_get_contents ($schema), true):
 			array();
 		
-		$return = Validator::make($input, $rules);
+		$validator = Validator::make ($input, $rules);
 		
-		$return->intersect = array_intersect_key($input, $rules);
+		$validator->intersect = array_intersect_key ($input, $rules);
 		
-		return $return;
+		return $validator;
 	}
-
+	
+	/**
+	 *	Get validated input
+	 *	This function is extended to Validator in bootstrap
+	 *
+	 *	@return array
+	 */
+	 public function getValidated()
+	 {
+		return array_intersect_key ($this->getData(), $this->getRules());
+	 }
+	 
 }
