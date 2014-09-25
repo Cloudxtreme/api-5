4e51392f51c03075ccce7626cf2ea9c5778c4434
Pedro Silva <pedrodee@gmail.com>
2014-09-25 11:45:56 +0100

    clean

--- app/controllers/ViewController.php
+++ app/controllers/ViewController.php
@@ -55,51 +55,11 @@ class ViewController extends BaseController {
 		return View::make('signin.logout', (array) $response);
 	}
 
-	public function register ()
-	{
-        $data = Input::all();
-
-        if( !empty($data) ){
-            // send request to engine via gearman
-            $output = App::make ('cwclient')->register ($data['email'], $data['password'], $data['firstname'], $data['name']);
-            // if ok redirect
-            if(isset($output['success'])){
-                return Redirect::to('login');
-            } else {
-                return View::make('signin.register', $output);
-            }
-        } else {
-            return View::make('signin.register', $data);
-        }
-	}
-
 	public function lostPassword ()
 	{
         $data = array();
 		return View::make('signin.lost_password', $data);
 	}
 
-	public function email ()
-	{
-        $data = array();
-        Mail::send('emails.welcome', $data, function($message)
-        {
-            $message->to('pedrodee@gmail.com', 'John Smith')->subject('Welcome!');
-        });
-
-		return 'email sent!';
-	}
-
-	public function sms ()
-	{
-        $data = array();
-        $message = new Clickatell;
-
-        $status = $message->to(32476858168)
-            ->message('CW is watching you!')
-            ->send();
-
-		return print_r($status, true);
-	}
 
 }
