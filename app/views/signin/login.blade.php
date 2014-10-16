f363dcf8be2e92c553134a2e0ae59bec2a222fdb
Pedro Silva <pedrodee@gmail.com>
2014-10-16 11:51:23 +0100

    laravel form and csrf token

--- app/views/signin/login.blade.php
+++ app/views/signin/login.blade.php
@@ -4,28 +4,51 @@
 
 	<div class="staticmiddle">
 		<div class="staticlogo">
-			{{ HTML::image("assets/img/shield+name.png", "Cloudwalkers logo") }}
+			{{ HTML::image("assets/img/shield+name.png", "Cloudwalkers") }}
 		</div>
 
 		<h1>{{ trans('login.title') }}</h1>
 
 		<div class="whiteframe">
 			<div class="frametitle">{{ trans('login.login') }}</div>
-			<form role="form">
+
+			{{ Form::open(array(
+				'url' => '#',
+				'method' => 'post',
+                'accept-charset' => 'UTF-8',
+                'role' => 'form'))
+            }}
 			    <div class="form-group">
-				    <!--<label for="exampleInputEmail1">Email</label>-->
-				    <input type="email" class="form-control" id="exampleInputEmail1" placeholder="{{ trans('login.email') }}" required>
+				    {{ Form::email('email', '', array(
+                        'class'         => 'form-control',
+                        'placeholder'   => trans('login.email'),
+                        'required'      => 'required'
+                    )) }}
 			    </div>
 			    <div class="form-group">
-				   <!-- <label for="exampleInputEmail1">Password</label>-->
-				    <input type="password" class="form-control" id="exampleInputEmail1" placeholder="{{ trans('login.password') }}" required>
+				    {{ Form::password('password', array(
+                        'class'         => 'form-control',
+                        'placeholder'   => trans('login.password'),
+                        'required'      => 'required'
+                    )) }}
 			    </div>
 			    <br>
-			    <button type="submit" class="btn btn-color pull-right">{{ trans('login.login') }}</button>
+			    {{ Form::submit(trans('login.login'), array('class'=>'btn btn-color pull-right')) }}
+
 			    {{ HTML::linkAction('ViewController@forgotpassword', trans('login.forgot.password'), array(), array('class' => 'muted pull-left', 'id' => 'forgotpw')) }}
-			</form>
+
+			{{ Form::close() }}
+
 			<span class="clearfix"></span>
 		</div>
+
+            @if ( !empty ($error) )
+	            @foreach ($error as $message)
+		            <div>
+		                <p>{{ $message }}</p>
+		            </div>
+	            @endforeach
+            @endif
 
 		<div id="manuals" class="center">
 			<span class="ion-ios7-cloud-outline"></span>
@@ -34,49 +57,3 @@
 	</div>
 
 @stop
-
-{{--<article>--}}
-
-	{{--@if ( !empty ($error) )--}}
-	{{--@foreach ($error as $message)--}}
-	{{--<div class="alert alert-error">--}}
-		{{--<button class="close" data-dismiss="alert"></button>--}}
-		{{--<span><p>{{ $message }}</p></span>--}}
-	{{--</div>--}}
-	{{--@endforeach--}}
-	{{--@else--}}
-
-	{{--@endif--}}
-
-	{{--@if ( !empty ($msg) )--}}
-	{{--<div class="alert alert-error">--}}
-		{{--<button class="close" data-dismiss="alert"></button>--}}
-		{{--<span><p>{{ $msg }}</p></span>--}}
-	{{--</div>--}}
-	{{--<script>--}}
-        {{--//window.localStorage.setItem('access_token', '{{ $msg }}');--}}
-    {{--</script>--}}
-	{{--@else--}}
-
-	{{--@endif--}}
-
-    {{--<h1 class="pull-left">{{ trans('login.login') }}</h1>--}}
-
-    {{--<form class="form-signin pull-right" method="post" action="">--}}
-
-        {{--<input name="email" id='email' type="email" class="input-block-level" placeholder="{{ trans('login.email') }}">--}}
-        {{--<input name="password" id='password' type="password" class="input-block-level" placeholder="{{ trans('login.password') }}">--}}
-        {{--<input name="login" type="hidden" value="1" />--}}
-        {{--<input type="submit" value="{{ trans('login.login') }}" />--}}
-
-    {{--</form>--}}
-
-    {{--<div class="clear"></div>--}}
-
-{{--</article>--}}
-
-{{--<article>--}}
-    {{--{{ HTML::linkAction('ViewController@forgotpassword', trans('login.forgot.password'), array(), array('class' => 'forgot-link')) }}--}}
-{{--</article>--}}
-
-{{--@stop--}}
